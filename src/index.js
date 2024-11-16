b710bb84383d721ae676e26af5814551cd42eeff
Camilo Martinez <equiman@users.noreply.github.com>
2024-11-16 11:41:22 -0500

    fix: simple first example

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-const arrayIsEmpty = (array) => {
-    const isArray = Array.isArray(array);
-    const hasElements = Boolean(array.length);
-
-    return !isArray || !hasElements;
+const sayHelloInJS = (name) => {
+    console.log(`Hello ${name}`);
 };
 
-const array1 = [{ key: 'value' }]
-const test1 = arrayIsEmpty(array1)
-console.log({ result: test1 })
-
-const array2 = [];
-const test2 = arrayIsEmpty(array2)
-console.log({ result: test2 })
-
-const array3 = 'a';
-const test3 = arrayIsEmpty(array3)
-console.log({ result: test3 })
-
-const array4 = ['a'];
-const test4 = arrayIsEmpty(array4)
-console.log({ result: test4 })
+const jsDev = 'Equiman';
+sayHelloInJS(jsDev);
