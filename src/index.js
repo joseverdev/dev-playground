4c4e8e00079a35a025b6c6ec7d60d70574b552e1
Camilo Martinez <equiman@users.noreply.github.com>
2024-11-16 00:06:14 -0500

    chore: format files

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const arrayIsEmpty = (array) => {
     return !isArray || !hasElements;
 };
 
-const array1 = [ { key: 'value' }]
+const array1 = [{ key: 'value' }]
 const test1 = arrayIsEmpty(array1)
-console.log({result: test1})
+console.log({ result: test1 })
 
 const array2 = [];
 const test2 = arrayIsEmpty(array2)
-console.log({result: test2})
+console.log({ result: test2 })
 
 const array3 = 'a';
 const test3 = arrayIsEmpty(array3)
-console.log({result: test3})
+console.log({ result: test3 })
 
 const array4 = ['a'];
 const test4 = arrayIsEmpty(array4)
-console.log({result: test4})
+console.log({ result: test4 })
